4475f55005f5a1cbb9b80db68e0c131b139f31e1
Cameron Sim <62146817+noremacsim@users.noreply.github.com>
2023-05-01 11:13:06 +0100

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,22 @@
+<!--    Title, Link, Fullscreen (True/false), ImageSrc-->
+
+window.stream = [
+    ['Netflix', 'https://netflix.com', true, 'https://yt3.googleusercontent.com/ytc/AL5GRJVNWToqHgCO1ez9xLj3HeIEsDlr1nnqH3n9P51weg=s900-c-k-c0x00ffffff-no-rj'],
+    ['Plex', 'https://app.plex.tv/desktop/#!/', true, 'https://play-lh.googleusercontent.com/slZYN_wnlAZ4BmyTZZakwfwAGm8JE5btL7u7AifhqCtUuxhtVVxQ1mcgpGOYC7MsAaU'],
+    ['Youtube', 'https://youtube.com', false, 'https://www.youtube.com/img/desktop/yt_1200.png'],
+    ['BBC Iplayer', 'https://www.bbc.co.uk/iplayer', true, 'https://pbs.twimg.com/profile_images/1450745962016686085/PFHb1vtn_400x400.jpg'],
+    ['Disney Plus', 'https://www.disneyplus.com/en-gb', true, 'https://yt3.googleusercontent.com/dLcy6GV425b8lHf1uLwsySvxopw8IhxbR-92U86hg8-hE77NIDq_3XTG2vQ6YQj7-uo-UW4N=s900-c-k-c0x00ffffff-no-rj'],
+    ['STV Player', 'https://player.stv.tv/', true, 'https://m.media-amazon.com/images/I/61TW-Oksl-L.png'],
+];
+
+window.game = [
+    ['Xbox Cloud Gaming', 'https://www.xbox.com/en-us/play', true, 'https://assets-www.xbox.com/xbox-web/static/media/CloudGaming_LetterBox.scale-200.ef909bf4.png'],
+    ['GForce Now', 'https://play.geforcenow.com/mall/#/layout/games', true, 'https://downloadr2.apkmirror.com/wp-content/uploads/2022/12/65/63a0aff2ec20c.png'],
+
+]
+
+window.browse = [
+    ['Facebook', 'https://facebook.com', false, 'https://cdn2.downdetector.com/static/uploads/logo/FB-f-Logo__blue_512.png'],
+    ['Messenger', 'https://www.messenger.com/', false, 'https://static.xx.fbcdn.net/rsrc.php/v3/y2/r/yvbOx5two0W.png'],
+    ['Waze', 'https://teslawaze.azurewebsites.net/', false, 'https://pbs.twimg.com/profile_images/1611472516920520705/gWLeDDCJ_400x400.jpg']
+]
