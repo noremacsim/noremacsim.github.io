b09346a417ab36b9cd73da1c36b6f82c96473d16
Cameron Sim <62146817+noremacsim@users.noreply.github.com>
2023-05-05 13:14:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,21 @@ const CACHE = "pwabuilder-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+const CACHE = "teslaApps";
+const offlineFallbackPage = "/index.html";
+
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
 });
 
 workbox.routing.registerRoute(
