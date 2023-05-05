3008412187988f813f3eeea08a2aee757f52145b
noremacsim <mrcameronsim@gmail.com>
2023-05-05 22:04:45 +0100

    1.5/6: - Update Service Workers

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,8 +1,10 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// This is the "Offline page" service worker
 
-const CACHE = "teslaApps";
-const offlineFallbackPage = "/index.html";
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
+const CACHE = "SimFlix";
+
+const offlineFallbackPage = "/offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -12,14 +14,31 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      caches.open(CACHE)
+          .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        return await fetch(event.request);
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        return await cache.match(offlineFallbackPage);
+      }
+    })());
+  }
+});
