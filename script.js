f3dd04afa3e2af33553a2d90d18fbeaecd42f109
Cameron Sim <62146817+noremacsim@users.noreply.github.com>
2023-05-05 20:35:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,41 @@
-$(document).ready(function () {
-    getFromStorage();
+$(document).ready(async function () {
+
+    await getFromStorage().then(data => {
+        addToStorage();
+    });
+
     if (window.newversion_a === null) {
         $('#helpModal').modal('show');
         localStorage.setItem(`${window.version}-pop`, 'true');
     }
+
 });
 
 function buildAppTabs() {
-    const stream = window.stream;
-    const games = window.game;
-    const browse = window.browse;
+    for (let appType in window.apps) {
+        let appTab = $(`#${appType}HTML`);
+        appTab.html('');
 
-    $('#streamHTML').html('');
-    $('#gameHTML').html('');
-    $('#browseHTML').html('');
+        let index = 0;
+        if (window.apps[appType].length > 0) {
+            for (const element of window.apps[appType]) {
+                appTab.append(addBlock(element[0], element[1], element[3], index, appType));
+                index++;
+            }
 
-    // Build Stream Apps
-    let streamIndex = 0;
-    if (stream.length > 0) {
-        for (const element of stream) {
-            $('#streamHTML').append(addBlock(element[0], element[1], element[3], streamIndex, 'stream'));
-            streamIndex++;
+            appTab.append(`
+                <div class="d-inline-flex position-relative p-2 newAppModalButton" data-type="${appType}">
+                    <img class="rounded-9 shadow-4" src="https://static.thenounproject.com/png/953211-200.png" alt="Search" style="width: 100px;height: 100px;background: #c1c1c1;border: 1px solid black;">
+                </div>
+            `);
+
+            appTab.append(`
+                <div class="d-inline-flex position-relative p-2" onclick="navigate('https://www.paypal.com/donate/?hosted_button_id=RAFQ4WLY2NX62', 'Donate')">
+                    <img class="rounded-9 shadow-4" src="https://www.littlewings.org.au/wp-content/uploads/bb-plugin/cache/72-727103_paypal-donate-button-png-transparent-png-200x200-square.png" alt="Donate" style="width: 100px; height: 100px;">
+                </div>
+            `);
         }
-    } else {
-        $('#tabStream').hide();
     }
-
-    // Build Game Apps
-    let gameIndex = 0;
-    if (games.length > 0) {
-        for (const element of games) {
-            $('#gameHTML').append(addBlock(element[0], element[1], element[3], gameIndex, 'game'));
-            gameIndex++;
-        }
-    } else {
-        $('#tabGame').hide();
-    }
-
-    // Build Browse Apps
-    let browseIndex = 0;
-    if (browse.length > 0) {
-        for (const element of browse) {
-            $('#browseHTML').append(addBlock(element[0], element[1], element[3], browseIndex, 'browse'));
-            browseIndex++;
-        }
-        let search = `
-            <div class="d-inline-flex position-relative p-2" data-mdb-toggle="modal" data-mdb-target="#searchModal">
-                <img class="rounded-9 shadow-4" src="https://images-eds-ssl.xboxlive.com/image?url=rOOPPWX9ohGfLksSzq1Wig8iTZ05uPbeb.DpV4tdbbbWNm1XXu8uEEIOyHxIQzbkg6QV19S8ccQ3rJJvQNmSUn_TC6y.r4fl_f9KOa1NN_xM_dIQYqurJp4Hx5.6pahDol7yzO6kUj5dDpUHeMO8DeUy4j1j3nWAb3FpbQDy62HqNt5tJA0oumeD.AUXipL0nv4zVEIvmPNRCpXAvcHAbPOseEO1d2lZRQuCAcZx5PzYaJwzPkskEea.PaAFnZB5kF_n8MfizglWus42hA4uvITSxw87Ms9MK99SID90opDn9szswm3koPJ.FRA_I_tR&format=source&w=120" alt="Search" style="width: 100px; height: 100px;">
-            </div>`;
-
-        $('#browseHTML').prepend(search);
-    } else {
-        $('#tabBrowse').hide();
-    }
-
-    //Paypal Donate
-    let paypalHtml = `
-    <div class="d-inline-flex position-relative p-2" onclick="navigate('https://www.paypal.com/donate/?hosted_button_id=RAFQ4WLY2NX62', 'Donate')">
-        <img class="rounded-9 shadow-4" src="https://www.littlewings.org.au/wp-content/uploads/bb-plugin/cache/72-727103_paypal-donate-button-png-transparent-png-200x200-square.png" alt="Donate" style="width: 100px; height: 100px;">
-    </div>
-    `;
-    $('#browseHTML').append(paypalHtml);
-    $('#streamHTML').append(paypalHtml);
-    $('#gameHTML').append(paypalHtml);
-
-
-    $('#streamHTML').append(`
-        <div class="d-inline-flex position-relative p-2 newAppModalButton" data-type="stream">
-            <img class="rounded-9 shadow-4" src="https://static.thenounproject.com/png/953211-200.png" alt="Search" style="width: 100px;height: 100px;background: #c1c1c1;border: 1px solid black;">
-        </div>
-    `);
-
-    $('#gameHTML').append(`
-        <div class="d-inline-flex position-relative p-2 newAppModalButton" data-type="game">
-            <img class="rounded-9 shadow-4" src="https://static.thenounproject.com/png/953211-200.png" alt="Search" style="width: 100px;height: 100px;background: #c1c1c1;border: 1px solid black;">
-        </div>
-    `);
-
-    $('#browseHTML').append(`
-        <div class="d-inline-flex position-relative p-2 newAppModalButton" data-type="browse">
-            <img class="rounded-9 shadow-4" src="https://static.thenounproject.com/png/953211-200.png" alt="Search" style="width: 100px;height: 100px;background: #c1c1c1;border: 1px solid black;">
-        </div>
-    `);
-
 }
 
 function addBlock(title, link, image, index, type) {
@@ -130,45 +83,28 @@ function navigate(link, title) {
 }
 
 function addToStorage() {
-    const stream = window.stream;
-    const games = window.game;
-    const browse = window.browse;
-
-    localStorage.setItem("stream", JSON.stringify(stream));
-    localStorage.setItem("game", JSON.stringify(games));
-    localStorage.setItem("browse", JSON.stringify(browse));
+    localStorage.setItem("apps", JSON.stringify(window.apps));
     buildAppTabs();
 }
 
 function resetData() {
-    window.stream = window.core.stream;
-    window.game = window.core.game;
-    window.browse = window.core.browse;
+    window.apps = coreApps;
     addToStorage();
     location.reload();
 }
 
 async function getFromStorage() {
-    return new Promise((resolve, reject) => {
-        if ("stream" in localStorage) {
-            window.stream = JSON.parse(localStorage.getItem("stream"));
-        } else {
-            window.stream = window.core.stream;
-        }
 
-        if ("game" in localStorage) {
-            window.game = JSON.parse(localStorage.getItem("game"));
+    return new Promise(async (resolve, reject) => {
+        if ("apps" in localStorage) {
+            if (JSON.parse(localStorage.getItem("apps")).length < 1) {
+                window.apps = coreApps;
+            } else {
+                window.apps = await JSON.parse(localStorage.getItem("apps"));
+            }
         } else {
-            window.game = window.core.game;
+            window.apps = coreApps;
         }
-
-        if ("browse" in localStorage) {
-            window.browse = JSON.parse(localStorage.getItem("browse"));
-        } else {
-            window.browse = window.core.browse;
-        }
-
-        addToStorage();
         resolve(true);
     });
 }
@@ -176,13 +112,11 @@ async function getFromStorage() {
 function saveCustom(title, link, type) {
     let app = [title, link, true, createAppImage(title), false];
 
-    if (type === 'stream') {
-        window.stream.push(app);
-    } else if (type === 'game') {
-        window.game.push(app);
-    } else if (type === 'browse') {
-        window.browse.push(app);
+    if (typeof window.apps[type] == "undefined") {
+        window.apps[type] = [];
     }
+
+    window.apps[type].push(app);
     addToStorage();
     $('#newAppModal').modal('hide');
 }
@@ -224,6 +158,23 @@ function cancelDeletes() {
     addToStorage();
 }
 
+async function importAppList(id) {
+    await getLayoutShare(id).then(async data => {
+        Swal.fire({
+            icon: 'success',
+            text: 'Successfully Imported App List',
+            target: 'body',
+            // toast: true,
+            position: 'center',
+            timer: 3000,
+            allowOutsideClick: true,
+            showConfirmButton: false
+        });
+        window.apps = data;
+        addToStorage();
+    })
+}
+
 $(document.body).on('taphold', '.appLink', function(e) {
     e.preventDefault();
     $('.deleteApp').show(200);
@@ -248,11 +199,11 @@ $(document.body).on('tap', '.deleteApp', function(e) {
     let type = $(this).attr("data-type");
 
     if (type === 'stream') {
-        window.stream.splice(index, 1);
+        window.apps.stream.splice(index, 1);
     } else if (type === 'game') {
-        window.game.splice(index, 1);
+        window.apps.game.splice(index, 1);
     } else if (type === 'browse') {
-        window.browse.splice(index, 1);
+        window.apps.browse.splice(index, 1);
     }
 
     $(`#${type}-${index}`).remove();
@@ -263,4 +214,30 @@ $(document.body).on('tap', '.newAppModalButton', function(e) {
     let type = $(this).attr("data-type");
     $('#newSiteType').val(type);
     $('#newAppModal').modal('show');
+});
+
+$(document.body).on('tap', '#generateCode', async function (e) {
+    e.preventDefault();
+    let code = 'ERROR';
+
+    $('#importInput').hide();
+    $('#appCode').show();
+    $('#appCode').prop("readonly", true);
+    $('#appCode').css('background', 'transparent');
+
+
+    await generateLayoutShare().then(data => {
+        if (data !== 'ERROR') {
+            const codeSplit = data.split("basket: ");
+            code = codeSplit[1];
+        }
+
+        $('#appCode').val(code.slice(0, -1));
+    });
+});
+
+$(document.body).on('tap', '#importCode', async function (e) {
+    e.preventDefault();
+    $('#appCode').hide();
+    $('#importInput').show();
 });
